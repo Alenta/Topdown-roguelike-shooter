38aa09ad39bce755ced67f9e3aafabe89c3ac360
Skaltan <thomasdavidsen@live.com>
2018-02-17 16:29:59 +0100

    inventory

---
+++ Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour {
+
+    public bool inventory;      //if true then this object can be stored in inventory
+    public bool openable;       //if this object can be opened
+    public bool locked;         //if this is true then object is locked
+    public GameObject ItemNeeded;       //item needed in order to interact with this item
+
+
+	void DoInteraction()
+    {
+        //''picked up''
+        gameObject.SetActive(false);
+    }
+}
